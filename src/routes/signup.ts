12ccdb39641c7910eb204517906735d1630967b3
eliyahukoren <elik.koren@gmail.com>
2023-02-20 09:42:21 +0200

    use custom error handler classes

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 const validators = () => {
@@ -21,14 +23,15 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
-    const { email, password } = req.body;
+    // const { email, password } = req.body;
 
     console.log("Creating user ...");
+    throw new DatabaseConnectionError();
 
-    res.send({});
+    //res.send({});
   }
 );
 
