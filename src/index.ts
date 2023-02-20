952cce475593f878c3bbda0562ce47c93094bf5d
eliyahukoren <elik.koren@gmail.com>
2023-02-20 08:16:35 +0200

    init routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import { json } from "body-parser";
 import express from "express";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there");
-});
+// define routes
+app.use( currentUserRouter );
+app.use( signInRouter );
+app.use( signOutRouter );
+app.use( signUpRouter );
 
 app.listen(3000, () => {
   console.log("listening port 3000.");
