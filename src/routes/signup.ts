9a4f25214f8b5f6b729570ed25993476a21e1ac2
eliyahukoren <elik.koren@gmail.com>
2023-02-21 15:13:35 +0200

    implement sign up login in the signup route

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/users";
 
 const router = express.Router();
 const validators = () => {
@@ -26,12 +27,17 @@ router.post(
       throw new RequestValidationError(errors.array());
     }
 
-    // const { email, password } = req.body;
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({email});
 
-    console.log("Creating user ...");
-    throw new DatabaseConnectionError();
+    if( existingUser ){
+      throw new BadRequestError("Email in use");
+    }
 
-    //res.send({});
+    const user = User.build({email, password});
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
