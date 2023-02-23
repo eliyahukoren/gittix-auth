e9749c977b9079cd952f356801fd7a1dc3226c1d
eliyahukoren <elik.koren@gmail.com>
2023-02-23 12:22:19 +0200

    add midlleware cookie-session

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { json } from "body-parser";
+import cookieSession from "cookie-session";
 import express from "express";
 import 'express-async-errors';
 import mongoose from 'mongoose';
@@ -11,7 +12,14 @@ import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
 
 const app = express();
+
+app.set('trust proxy', true);
+
 app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true,
+}));
 
 // define routes
 app.use( currentUserRouter );
