2701a15ba866d6ae865037793c7a6fa4a64a08fe
eliyahukoren <elik.koren@gmail.com>
2023-02-23 12:29:44 +0200

    use jwt and save in cookie object

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/users";
@@ -36,6 +37,17 @@ router.post(
 
     const user = User.build({email, password});
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, 'secret/key');
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt
+    };
 
     res.status(201).send(user);
   }
