13e793d9c07a7a2c7595516b75ddbe2be5bae76a
eliyahukoren <elik.koren@gmail.com>
2023-03-06 09:42:33 +0200

    add validate request and body middlewares

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from "express";
+import { validateBody } from "../middlewares/validate-body";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req: Request, res: Response) => {
-  res.send("Hi There!");
-});
+router.post(
+  "/api/users/signin",
+  validateBody("Email must be valid", "You must supply password"),
+  validateRequest,
+  (req: Request, res: Response) => {}
+);
 
 export { router as signInRouter };
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,55 +1,43 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
 import { BadRequestError } from "../errors/bad-request-error";
-import { RequestValidationError } from "../errors/request-validation-error";
+
+import { validateBody } from "../middlewares/validate-body";
+import { validateRequest } from "../middlewares/validate-request";
 import { User } from "../models/users";
 
 const router = express.Router();
-const validators = () => {
-  return [
-    body('email')
-      .isEmail()
-      .withMessage('Email must be valid'),
-    body('password')
-      .trim()
-      .isLength({min: 4, max: 20})
-      .withMessage('Password must be between 4 and 20 characters')
-  ];
-}
 
 router.post(
   "/api/users/signup",
-  validators(),
+  validateBody(
+    "Email must be valid",
+    "Password must be between 4 and 20 characters"
+  ),
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
-    const existingUser = await User.findOne({email});
+    const existingUser = await User.findOne({ email });
 
-    if( existingUser ){
+    if (existingUser) {
       throw new BadRequestError("Email in use");
     }
 
-    const user = User.build({email, password});
+    const user = User.build({ email, password });
     await user.save();
 
     // Generate JWT
     const userJwt = jwt.sign(
       {
         id: user.id,
-        email: user.email
+        email: user.email,
       },
       process.env.JWT_KEY!
     );
 
     // Store it on session object
     req.session = {
-      jwt: userJwt
+      jwt: userJwt,
     };
 
     res.status(201).send(user);
