a42f97d4b88b2463ead1d1afcdf69d73b9034428
eliyahukoren <elik.koren@gmail.com>
2023-03-06 11:46:40 +0200

    refactor index to app

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import express from "express";
+import "express-async-errors";
+
+
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/error-handler";
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+// define routes
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+// define error handle middleware
+app.use(errorHandler);
+
+export { app };
+
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,22 @@
-import { json } from "body-parser";
-import cookieSession from "cookie-session";
-import express from "express";
-import 'express-async-errors';
-import mongoose from 'mongoose';
-
-import { NotFoundError } from "./errors/not-found-error";
-import { errorHandler } from "./middlewares/error-handler";
-import { currentUserRouter } from "./routes/current-user";
-import { signInRouter } from "./routes/signin";
-import { signOutRouter } from "./routes/signout";
-import { signUpRouter } from "./routes/signup";
-
-const app = express();
-
-app.set('trust proxy', true);
-
-app.use(json());
-app.use(cookieSession({
-  signed: false,
-  secure: true,
-}));
-
-// define routes
-app.use( currentUserRouter );
-app.use( signInRouter );
-app.use( signOutRouter );
-app.use( signUpRouter );
-
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-// define error handle middleware
-app.use( errorHandler );
+import mongoose from "mongoose";
+import { app } from "./app";
 
 const startUp = async () => {
-
-  if( !process.env.JWT_KEY){
+  if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined.");
   }
 
-  try{
-    mongoose.set('strictQuery', true);
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    console.log('Connected to MongoDB.')
-  }catch(err){
-    console.error(err)
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB.");
+  } catch (err) {
+    console.error(err);
   }
 
   app.listen(3000, () => {
     console.log("Auth listening port 3000.");
   });
-
 };
 
 startUp();
-
-
